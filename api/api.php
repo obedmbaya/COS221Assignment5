d9962f9ade1c00451c0bd5b503708dbc814be7ab
Jared <jaredluke3@gmail.com>
2025-05-05 19:30:28 +0200

    made api.php

---
+++ api/api.php
@@ -0,0 +1,37 @@
+<?php
+
+    require_once("signuplogin.php");
+    require_once("search.php");
+    require_once("signup.php");
+
+    $json = file_get_contents('php://input');
+    $data = json_decode($json, true);
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST"){
+
+        if (isset($data["type"]) && $data["type"] === "Signup"){
+
+            handleSignup($data);
+
+        } else if (isset($data["type"]) && $data["type"] === "Login"){
+
+            handleLogin($data);
+
+        } else if (isset($data["type"]) && $data["type"] === "Register"){
+
+            handleSearch($data);
+
+        }
+
+
+    } else {
+        header("HTTP/1.1 400 Bad Request");
+        header("Content-type application/json");
+        echo json_encode([
+            "status" => "success",
+            "timestamp" => $timestamp,
+            "data" => $results
+        ]);
+    }
+
+?>
