64268e029b9d7865782f369cfb53b5b670d82e54
hixerydixery <mahabeerjoshua@gmail.com>
2025-05-06 13:38:50 +0200

    Search API now formatted into seperation of concerns

--- api/search.php
+++ api/search.php
@@ -1,55 +1,6 @@
 <?php
 
-header('Content-Type' : 'application/json');
-
-//connect to our database
-require_once 'config.php';
-
-class APIHandler
-{
-    private $database;
-
-    public function __construct($conn){
-        $this->database = $conn;
-    }
-
-    private function validateAPIKey($key){
-        $stmt = $this->database->prepare("SELECT ApiKey FROM ApiKey WHERE ApiKey = ?");
-        $stmt->bind_param("s", $key);
-        $stmt->execute();
-        $stmt->store_result();
-
-        //check if there is a matching key in the database
-        return $stmt->num_rows > 0;
-    }
-
-    public function requestType(){
-        $json = file_get_contents("php://input");
-        $data = json_decode($json, true);
-
-        if (!$this->validateAPIKey($data["apiKey"]) || !isset($data["apikey"])){
-            $this->sendResponse("error", "Invalid API Key", 403);
-            return;
-        }
-
-        //different types still need to be implemented. 
-        //including: register, login, logout, ect...
-
-        $type = $data["type"];
-
-        if ($type === "search"){
-            $this->search($input);
-        }
-
-        //-------------->>>>>>>>>>> implementation for different request types
-
-        else{
-            $this->sendResponse("error", "Invalid Request Type", 400);
-        }
-
-    }
-
-    private function search($data){
+private function search($data){
         $query = "SELECT p.ProductID, p.ProductName, p.Description, p.Brand, p.IMG_Reference pp.Price, r.RetailerName
                   FROM Product p
                   JOIN ProductPrice pp ON p.ProductID = pp.ProductID
@@ -119,20 +70,5 @@ class APIHandler
         );
     }
 
-
-}
-
-try{
-    $apiHandler = new APIHandler($conn);
-    $apiHandler->requestType();
-}
-catch (Exception $e){
-    http_response_code(500);
-    echo json_encode([
-        "status" => "error",
-        "timestamp" => time(),
-        "data" => "Internal Server Error" . $e;
-    ])
-}
 
 ?>
