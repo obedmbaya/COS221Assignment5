bcf1b313cd7b44669173d76f9177d09399cc36c4
Obed Edom Mbaya <obed.mbaya@yahoo.com>
2025-05-12 23:51:58 +0200

    minimal logoit

--- api/logoutapi.php
+++ api/logoutapi.php
@@ -1,7 +1,7 @@
 <?php
 
 header("Content-Type", "application/json");
-require_once "/config.php";
+require_once "config.php";
 
 
 //user did not signup with correct method
@@ -18,7 +18,7 @@ $json = file_get_contents('php://input');
 $data = json_decode($json, true);
 
 if ($data['type'] === "Logout") {
-    if (empty($data['email'])) {
+    if (empty($data['api_key'])) {
         http_response_code(400);
         echo json_encode([
             "status" => "failed", 
@@ -27,25 +27,35 @@ if ($data['type'] === "Logout") {
         exit();
     }
 
-    $email = htmlspecialchars($data["email"]);
+    $api_key = htmlspecialchars($data["api_key"]);
 
-    $query = "SELECT * FROM users WHERE email = ?;";
-    $stmt_pwd = $database->prepare($query);
-    $stmt_pwd->execute([$data['email']]);
-    $usr = $stmt_pwd->fetch(PDO::FETCH_ASSOC);
+    $query = "SELECT * FROM users WHERE api_key = ?";
+    $stmt = $database->prepare($query);
+    $stmt->bind_param("s", $api_key);
+    $stmt->execute();
+    
+    $result = $stmt->get_result();
+    $usr = $result->fetch_assoc();
+    
+    $stmt->close();
 
     if ($usr) {
+
         session_start();
         session_destroy();
+        
         echo json_encode([
             "status" => "success",
+            "timestamp" => round(microtime(true) * 1000),
             "data" => "User logged out successfully"
         ]);
-    } else {
+    } 
+    
+    else {
         http_response_code(401);
         echo json_encode([
             "status" => "failed",
-            "data" => "User not found"
+            "data" => "Invalid API key"
         ]);
     }
 }
