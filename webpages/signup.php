1c1c8c9430abfed5bc4428ebb7c46d34f7dbb76b
mbalz916 <makopimbalithobejane@gmail.com>
2025-05-24 11:03:02 +0200

    modified title from login to signup

--- webpages/signup.php
+++ webpages/signup.php
@@ -1,12 +1,12 @@
 <?php
-$pageTitle="Login";
-$currentPage="login";
+$pageTitle="Signup"; 
+$currentPage="Signup";
 include 'header.php';
 ?>
 <head>
     <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width,initial-scale=1.0">
-<title>CompareIt - Login</title>
+<title>CompareIt - Signup</title> 
 
 <script src ="https://cdn.tailwindcss.com"></script>
  <script>
