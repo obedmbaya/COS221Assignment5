25d905413ce75b179ea38d9bf7c5e6c6b21b9d37
Finn <finnleyzenzo@gmail.com>
2025-05-27 01:58:06 +0200

    added getproducts endpoint

--- api/endpoints.php
+++ api/endpoints.php
@@ -885,6 +885,38 @@ function handleGetRetailerById(){
     ]);
 
 }
+function getAllProducts() {
+    $conn = Database::instance()->getConnection();
+
+    $query = "SELECT ProductID, ProductName, Description, Brand, IMG_Reference FROM Product";
+    $result = $conn->query($query);
+
+    if (!$result) {
+        http_response_code(500);
+        header('Content-Type: application/json');
+        echo json_encode([
+            "status" => "error",
+            "timestamp" =>time(),
+            "products" => []
+        ]);
+        exit;
+    }
+
+    $products = [];
+
+    while ($row = $result->fetch_assoc()) {
+        $products[] = $row;
+    }
+
+    http_response_code(200);
+    header('Content-Type: application/json');
+    echo json_encode([
+        "status" => "success",
+        "timestamp" => time(),
+        "products" => $products
+    ]);
+    exit;
+}
 
 // function validateApikey($data){
 
